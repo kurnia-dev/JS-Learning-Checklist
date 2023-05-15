13c0ee8e2d2282a0fcb3185dd87c151608e9284e
Kurnia <kurniawanviic54@gmail.com>
2023-05-15 20:03:59 +0700

    feat : add feature to save and load progress (checked input)

--- main.js
+++ main.js
@@ -26,10 +26,44 @@ function loadMaterials(Materials) {
         for (let i in Materials[prop]) {
             let label = document.createElement('label')
             let num = Number(i) + 1
-            label.innerHTML = `<input type="checkbox" id="${num}">${Materials[prop][i]}`
+            label.innerHTML = `<input type="checkbox" id="${prop + ' ' + num}">${Materials[prop][i]}`
             div.append(label)
         }
         
         container.append(div)
+    }
+
+    addEvent()
+}
+
+function addEvent() {
+    let checkbox = document.querySelectorAll('input')
+    checkbox.forEach(el => {
+        let id = el.getAttribute('id')
+        el.addEventListener('click', () => {
+            saveProgress(id)
+        })
+
+        loadProgress(el, id)
+    });
+}
+
+function saveProgress(id) { // To save the checked input to local storage
+    if (!localStorage.getItem('progress')) {
+        let arr = [id]
+        localStorage.setItem('progress', JSON.stringify(arr))
+    } else {
+        let arr = JSON.parse(localStorage.getItem('progress')) // array
+        // console.log(typeof get);
+        arr.push(id)
+        localStorage.setItem('progress', JSON.stringify(arr))
+    }
+}
+
+function loadProgress(el, id) { // to load checked input from local storage
+    let arr = JSON.parse(localStorage.getItem('progress'))     
+
+    for (let i in arr) {
+        if (arr[i] == id) el.setAttribute('checked', 'checked') 
     }
 }
